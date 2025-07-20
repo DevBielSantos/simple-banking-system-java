f1ff8ec3fa613942572d89e2d2510b743ab60aa3
DevBielSantos <devbielst@gmail.com>
2025-07-20 19:21:09 -0300

    feat: implemented Bank class

--- src/Bank.java
+++ src/Bank.java
@@ -1,3 +1,25 @@
-public class Bank {
+import java.util.ArrayList;
 
+public class Bank {
+    private ArraysList<Account> accounts = new ArrayList<>();
+
+    public void addAccount(Account account) {
+        accounts.add(account);
+    }
+
+    public Account findAccount(int number) {
+        for (Account acc : accounts) {
+            if (acc.getNumber() == number) {
+                retunr acc;
+            }
+        }
+        return null;
+    }
+    public void listAccounts() {
+        for (Account acc : accounts) {
+            System.out.println("Account " + acc.getNumber() +
+                               " | Owner: " + acc.getOwner() +
+                               " | Balance: " + acc.getBalance());
+        }
+    }
 }
